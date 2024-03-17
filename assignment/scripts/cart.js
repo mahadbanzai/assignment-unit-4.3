f3819c7ad421d1c78879b3e78cd256463c7c83b5
Mahad Hassan <mahadh578@gmail.com>
2024-03-17 18:54:12 -0500

    worked on carts again

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,6 +1,26 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket=[];
+function addItem(item){
+    basket.push(item);
+    return true;
+}
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+function listItems(){
+for(let i=0;i<basket.length;i++){
+    console.log(basket[i]);
+}
+}
+function empty(){
+    while (basket.length > 0) {
+    basket.pop();
+}
+}
+console.log(basket);
 
 
 
